f8d511368afa979a47472edfa0734d8ac9b79fac
kogleo <hellresiak@gmail.com>
2023-03-06 14:56:56 +0700

    add focus effect when render reservation page

--- src/components/date-picker/index.tsx
+++ src/components/date-picker/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { TextField } from '@mui/material';
 import dayjs, { Dayjs } from 'dayjs';
@@ -6,12 +6,16 @@ import styles from '@/components/reservation/styles.module.scss'
 import { useAppDispatch } from '@/hooks';
 import {datePicker } from '@/store/reducer/reseravation'
 
-
 type Props = {};
 
 export default function DatePicker(props: Props){
 
   const dispatch = useAppDispatch()
+  const inputReference = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+      inputReference.current?.focus();
+  }, []);
   const [value, setValue] = useState<Dayjs | null>(dayjs(new Date()));
 
   const handleChange = (newValue: Dayjs | null) => {
@@ -21,14 +25,15 @@ export default function DatePicker(props: Props){
     setValue(newValue);
   };
   return (
-    <DesktopDatePicker
-      className={styles.pickerContainer}
-      inputFormat="DD/MM/YYYY"
-      value={value}
-      closeOnSelect={false}
-      onChange={handleChange}
-      renderInput={(params) => <TextField {...params} />}
-    />
+      <DesktopDatePicker
+        inputRef={inputReference} 
+        className={styles.pickerContainer}
+        inputFormat="DD/MM/YYYY"
+        value={value}
+        closeOnSelect={false}
+        onChange={handleChange}
+        renderInput={(params) => <TextField  className='border-white focus-within:border-blue-400'  {...params} />}
+      />
   );
 };
 
