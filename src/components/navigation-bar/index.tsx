3d15ba88cf9185bea9c7320912301fc5cc1ec724
Bao Nguyen Thien <65020023+bao-nguyenbku@users.noreply.github.com>
2023-03-24 23:28:04 +0700

    update menu

--- src/components/navigation-bar/index.tsx
+++ src/components/navigation-bar/index.tsx
@@ -8,11 +8,12 @@ type Props = {
 
 const NavigationBar = (props: Props) => {
   const { scrollableNodeRef } = props;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [scrollTop, setScrollTop] = useState<number>(0);
   const handleScroll = useCallback((event: any) => {
     setScrollTop(event.target.scrollTop);
   }, []);
-  const isShowBackground = scrollTop > 200;
+  // const isShowBackground = scrollTop > 200;
   useEffect(() => {
     const div = scrollableNodeRef?.current;
     div?.getScrollElement()?.addEventListener('scroll', handleScroll, true);
@@ -22,20 +23,23 @@ const NavigationBar = (props: Props) => {
         ?.removeEventListener('scroll', handleScroll, true);
     };
   }, [scrollableNodeRef, handleScroll]);
-  const scrolledClass = isShowBackground
-    ? 'fixed bg-white/10 backdrop-blur-lg'
-    : 'absolute';
+  // const scrolledClass = isShowBackground
+  //   ? 'fixed bg-white/10 backdrop-blur-lg'
+  //   : 'absolute';
   return (
     <ul
-      className={`flex items-center justify-end text-white gap-8 right-0 py-6 px-20 w-full z-50 transition-all duration-700 uppercase ${scrolledClass}`}>
+      className={`flex items-center justify-end text-white gap-8 right-0 py-6 px-20 w-full z-50 transition-all duration-700 uppercase absolute`}>
       <li className='mr-auto font-passions-conflict text-5xl normal-case'>
         <Link href='/'>Satisfa</Link>
       </li>
       <li>
-        <Link href="#about-us">About us</Link>
+        <Link href="/#about-us">About us</Link>
       </li>
       <li>
         <Link href='/menu'>Our menu</Link>
+      </li>
+      <li>
+        <Link href='/reservation'>Reservation</Link>
       </li>
       <li>
         <Link href='#footer'>Contact</Link>
