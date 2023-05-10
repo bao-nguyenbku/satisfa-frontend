62815ee216943d52066d4c9cdb7ec6fbcf399faa
kogleo <hellresiak@gmail.com>
2023-05-11 03:51:48 +0700

    update responsive

--- src/components/payment/order-detail/order-item.tsx
+++ src/components/payment/order-detail/order-item.tsx
@@ -9,9 +9,6 @@ import { formatCurrency } from '@/utils';
 type Props = {
     item: CartItem;
 };
-
-const sampleImage =
-  'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80';
 export default function OrderItem(props: Props) {
   const { item } = props
   return (
